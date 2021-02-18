5ffdc0f0f39cbb360a2fcd5e0128a2b1eca007a9
Meinczinger, Tibor <tibor.meinczinger@merck.com>
2021-02-18 17:54:57 +0100

    Added comments

--- ai-nano-degree/project3/my_custom_player.py
+++ ai-nano-degree/project3/my_custom_player.py
@@ -4,8 +4,8 @@ import pickle
 from isolation import DebugState
 from collections import defaultdict, Counter
 
-MIN_DEPTH = 4
-MAX_DEPTH = 5
+MIN_DEPTH = 3
+MAX_DEPTH = 3
 MIN_NR_OF_GAMES = 3
 _WIDTH = 11
 
@@ -58,6 +58,7 @@ class CustomPlayer(DataPlayer):
                     action = max(self.data[state.board], key=lambda k: self.get_ratio(self.data[state.board][k]))
                     # Shall we use this action?
                     if self.use_action(self.data[state.board][action]):
+                        print("Using action, ply:", state.ply_count, "action:", action)
                         self.queue.put(action)
                     else:
                         if state.ply_count < 2:
@@ -75,6 +76,17 @@ class CustomPlayer(DataPlayer):
                     else:
                         self.find_actions(state)
 
+    """ Get the best (with highest win/loss ratio) first action from the book """
+    def get_best_first_actions(self, state):
+        d = DebugState()
+        actions = [a for a in self.data[state.board]]
+        actions.sort(reverse=True, key=lambda k: self.get_ratio(self.data[state.board][k]))
+        for a in actions:
+            print(a, self.data[state.board][a], self.data[state.board][a]['wins'] +
+                  self.data[state.board][a]['losses'], self.get_ratio(self.data[state.board][a]),
+                  d.from_state(state).ind2xy(a))
+
+    """ Decide whether an action from the opening book should be used """
     @staticmethod
     def use_action(results):
         # if less than MIN_NR_OF_GAMES played with this position, return 0
@@ -86,6 +98,7 @@ class CustomPlayer(DataPlayer):
                 return False
         return True
 
+    """ For an action from the book, get the ration of wins/losses """
     @staticmethod
     def get_ratio(results):
         # if less than MIN_NR_OF_GAMES played with this position, return 0
@@ -94,13 +107,16 @@ class CustomPlayer(DataPlayer):
         else:
             return results['wins'] / (results['wins'] + results['losses'])
 
+    """ Put a random action to the queue """
     def random_action(self, state):
         self.queue.put(random.choice(state.actions()))
 
+    """ Find action by running alpha-beta pruning with iterative deepening"""
     def find_actions(self, state):
         for i in range(MIN_DEPTH, MAX_DEPTH + 1):
             self.queue.put(self.alpha_beta(state, depth=i))
 
+    """ Get an action if the agent is used in simulation mode """
     def get_simulated_action(self, state):
         if state.ply_count < 2:
             # create the playbook
@@ -149,11 +165,13 @@ class CustomPlayer(DataPlayer):
             with open("data.pickle", 'wb') as f:
                 pickle.dump(data, f)
 
+    """ Alpha-beta pruning search """
     def alpha_beta(self, state, depth):
         alpha = -float("inf")
         beta = float("inf")
         return max(state.actions(), key=lambda x: self.beta(state.result(x), depth - 1, alpha, beta))
 
+    """ Alpha search """
     def alpha(self, state, depth, alpha, beta):
         if state.terminal_test():
             return state.utility(self.player_id)
@@ -167,6 +185,7 @@ class CustomPlayer(DataPlayer):
             alpha = max(alpha, value)
         return value
 
+    """ Beta search """
     def beta(self, state, depth, alpha, beta):
         if state.terminal_test():
             return state.utility(self.player_id)
@@ -180,6 +199,7 @@ class CustomPlayer(DataPlayer):
             beta = min(beta, value)
         return value
 
+    """ Get the score for a non-terminal state by applying heuristics"""
     def score(self, state):
         own_loc = state.locs[self.player_id]
         opp_loc = state.locs[1 - self.player_id]
@@ -188,15 +208,18 @@ class CustomPlayer(DataPlayer):
         return self.score_liberties(own_liberties, opp_liberties) + \
                self.score_distance_from_center(state, own_loc, opp_loc)
 
+    """ Heuristic based on the number of liberties """
     @staticmethod
     def score_liberties(own_liberties, opp_liberties):
         return len(own_liberties) - len(opp_liberties)
 
+    """ Heuristic based on the distance of the agent's location from the center """
     def score_distance_from_center(self, state, own_loc, opp_loc):
         own_distance_from_center = self.distance_from_the_center(int(own_loc)) / 5.0
         opp_distance_from_center = self.distance_from_the_center(int(opp_loc)) / 5.0
         return -own_distance_from_center + opp_distance_from_center
 
+    """ Convert location to x, y coordinates """
     @staticmethod
     def ind2xy(ind):
         """ Convert from board index value to xy coordinates
@@ -205,8 +228,9 @@ class CustomPlayer(DataPlayer):
         along the columns progressing towards the left, and y increasing along
         the rows progressing towards teh top.
         """
-        return (ind % (_WIDTH + 2), ind // (_WIDTH + 2))
+        return ind % (_WIDTH + 2), ind // (_WIDTH + 2)
 
+    """ Compute the manhatten distance of the location from the center """
     def distance_from_the_center(self, loc):
         x, y = self.ind2xy(loc)
         manhatten_distance = abs(5 - x) + abs(4 - y)
